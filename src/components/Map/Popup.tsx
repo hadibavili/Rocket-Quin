386c6f7490dd30e94e9b244644673d4c406b8f6a
hadibavili <bavilihadi@gmail.com>
2022-02-15 15:42:24 +0330

    popup element changed to h3

--- src/components/Map/Popup.tsx
+++ src/components/Map/Popup.tsx
@@ -7,10 +7,10 @@ interface PopupProps {
 const Popup: React.FC<PopupProps> = ({ rocket }) => {
    return (
       <LeafletPopup>
-         <h6>Name: {rocket.name}</h6>
-         <h6>Name of lunch: {rocket.pad.name}</h6>
-         <h6>Lunch date: {rocket.window_start}</h6>
-         <h6>Agencies: {rocket.launch_service_provider.name}</h6>
+         <h3>Name: {rocket.name}</h3>
+         <h3>Name of lunch: {rocket.pad.name}</h3>
+         <h3>Lunch date: {rocket.window_start}</h3>
+         <h3>Agencies: {rocket.launch_service_provider.name}</h3>
       </LeafletPopup>
    );
 };
